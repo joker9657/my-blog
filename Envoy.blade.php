485199d6bc1d1dfb385bb52708261d919f3cab31
qf-Z <zqf2102398@gmail.com>
2020-04-21 21:05:28 +0800

    feat add envoy

---
+++ Envoy.blade.php
@@ -0,0 +1,7 @@
+@servers(['web' => ['zqf@117.50.90.220']])
+
+@task('deploy', ['on' => 'web', 'confirm' => true])
+    cd /home/zqf/Code/my-blog
+    git pull
+    php artisan opcache:clear
+@endtask
