1888273a670ad7eb3cb2a28b2899d8392a48dff6
qf-Z <zqf2102398@gmail.com>
2020-04-21 21:48:10 +0800

    update about me js typewriter effect

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,4 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
 @extends('layout.app')
 @section('main')
     <main>
@@ -7,7 +8,7 @@
 {{--            <p>瘦弱似条竹竿</p>--}}
 {{--            <p>广州不知名二本毕业</p>--}}
 {{--            <p>至今仍是三流码农</p>--}}
-
+            <span id="typed" style="white-space: pre-wrap;line-height: 30px;"></span>
         </div>
         <div class="right-main">
             <div class="sidebar">
@@ -46,3 +47,18 @@
         </div>
     </main>
 @stop
+<script !src="">
+    window.onload = function () {
+        let options = {
+            strings: [
+                '18年羊城不知名本科毕业，一直从事后端开发，熟练掌握 Linux/\Nginx/\PHP/\MySQL/\Laravel/\Redis/\Docker 的拼写。',
+                '喜欢上网冲浪和骑行。 ^1000',
+                "这里主要记录本人技术栈相关内容，有事可以发送邮件至 zqf2102398#gmail.com。",
+            ],
+            typeSpeed: 100,   //打印速度
+            startDelay: 300, //开始之前的延迟300毫秒
+            loop: false,      //是否循环
+        };
+        let typed = new Typed('#typed', options);
+    }
+</script>
