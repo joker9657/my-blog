116e0e817c252f4255e939ed513553910d6fe0ea
joker9657 <zqf2102398@gmail.com>
2020-10-17 15:30:05 +0800

    update with the last

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -16,20 +16,20 @@
     wx.config(config);
 
     wx.ready(function () {
-        wx.getLocation({
-            type: 'gcj02',
-            success: function (res) {
-                latitude = res.latitude;
-                longitude = res.longitude;
-                console.log(latitude);
-            }
-        })
+        // wx.getLocation({
+        //     type: 'wgs84',
+        //     success: function (res) {
+        //         latitude = res.latitude;
+        //         longitude = res.longitude;
+        //         console.log(latitude);
+        //     }
+        // })
         wx.openLocation({
-            latitude: 23.08383, // 纬度，浮点数，范围为90 ~ -90
-            longitude: 114.38273, // 经度，浮点数，范围为180 ~ -180。
+            latitude: 23.153631, // 纬度，浮点数，范围为90 ~ -90
+            longitude: 114.417440, // 经度，浮点数，范围为180 ~ -180。
             name: '康泰家居广场', // 位置名
             address: '广东省惠州市惠城区站前一路7-1号', // 地址详情说明
-            scale: 20, // 地图缩放级别,整形值,范围从1~28。默认为最大
+            scale: 16, // 地图缩放级别,整形值,范围从1~28。默认为最大
             infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
         });
     });
